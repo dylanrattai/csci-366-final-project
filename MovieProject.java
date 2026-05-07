886b49e3eaaaa6a6b0fa8f92fe6f797a589705f5
awday1 <awday1@gmail.com>
2026-05-07 17:22:44 -0500

    Contains my code for the functions and GUI that implements those functions

---
+++ MovieProject.java
@@ -0,0 +1,24 @@
+
+package movieproject;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * Main method, run to run the project
+ * 
+ * @author awday
+ */
+public class MovieProject {
+
+
+    public static void main(String[] args) {
+       
+        GUI.main(args);
+        
+        
+    }
+    
+}
